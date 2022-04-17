d1a80155f611d5bdd949af40ffdee0dd2e9425f4
Karen <krufino@itech-sac.com>
2022-04-17 01:20:16 -0500

    primer commit

---
+++ labnotes/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyCty5HkD8RFVk5q0XSL1AAwMcYx-0THviE",
+    authDomain: "socialnetwork-ea46e.firebaseapp.com",
+    projectId: "socialnetwork-ea46e",
+    storageBucket: "socialnetwork-ea46e.appspot.com",
+    messagingSenderId: "1012336433769",
+    appId: "1:1012336433769:web:890a248beeaef0eed8a0f8",
+    measurementId: "G-3MPHV4RY5H"
+  },
+  production: true
+};
---
+++ labnotes/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCty5HkD8RFVk5q0XSL1AAwMcYx-0THviE",
+    authDomain: "socialnetwork-ea46e.firebaseapp.com",
+    projectId: "socialnetwork-ea46e",
+    storageBucket: "socialnetwork-ea46e.appspot.com",
+    messagingSenderId: "1012336433769",
+    appId: "1:1012336433769:web:890a248beeaef0eed8a0f8",
+    measurementId: "G-3MPHV4RY5H"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
