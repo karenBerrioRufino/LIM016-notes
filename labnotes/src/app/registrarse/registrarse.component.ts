a0c428225238a9ba8e975c290202ee01d273e6b1
Karen <krufino@itech-sac.com>
2022-04-23 01:15:24 -0500

    create user

---
+++ labnotes/src/app/registrarse/registrarse.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { CreateusersService } from '../services/createusers.service';
+/* import { user } from '@angular/fire/auth';
+
+import { AngularFirestore } from '@angular/fire/compat/firestore'; */
+
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-registrarse',
+  templateUrl: './registrarse.component.html',
+  styleUrls: ['./registrarse.component.scss']
+})
+export class RegistrarseComponent implements OnInit {
+
+  changeIcon: boolean = false;
+  showText: boolean = false;
+  usuario: any = {
+    username: '',
+    email: '',
+    password: ''
+  }
+  form!: FormGroup;
+  constructor(
+    // firestore : AngularFirestore
+    private createusersService: CreateusersService
+  ) { }
+
+  ngOnInit(): void {}
+
+  seePassword(){
+    this.changeIcon = !this.changeIcon;
+    this.showText = !this.showText;
+  }
+
+  register(){
+    console.log(this.usuario) 
+    const {email, password} = this.usuario;
+    this.createusersService.registerInFirestore(email, password)
+    .then(res => {
+      delete this.usuario.password;
+      console.log('se guardó en auth', res)
+      this.createusersService.saveUser(this.usuario, res?.user?.uid).then(() => {
+        console.log('Usuario registrado');
+        // this.form.reset();
+      }, error => {
+        console.log('Opps.. ocurrio un error', error);
+      })
+    });
+  }
+}
